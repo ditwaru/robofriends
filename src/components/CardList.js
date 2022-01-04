eaf7b15ace38cd0fb29f874dc6415711f98aa02f
jpegboi <danitwaru@gmail.com>
2022-01-04 16:32:11 -0500

    robofriends first draft

---
+++ src/components/CardList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Card from './Card'
+const CardList = ({ robots }) => {
+    return <div>{robots.map(({id, name, email}) => <Card key={id} id={id} name={name} email={email} />)}</div>
+}
+
+export default CardList;
