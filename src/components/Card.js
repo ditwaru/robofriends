4358301fdcd4f2fa6ef1d12d4557885a33207cef
daniel.itwaru@elevationchurch.org <daniel.itwaru@elevationchurch.org>
2022-01-11 16:29:38 -0500

    meta and title

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-const Card = ({name, id, email}) => {
-    return (
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc'>
-            <img src={`https://robohash.org/${id}?200x200`} height="200" width="200" alt="robots"/>
-            <div>
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        </div>
-    )
-}
+const Card = ({ name, id, email }) => {
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+      <img
+        src={`https://robohash.org/${id}?size=200x200`}
+        height="200"
+        width="200"
+        alt="robots"
+      />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
